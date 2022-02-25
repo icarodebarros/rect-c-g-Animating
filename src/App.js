7b21f539d6f1f2d8e8a017e95da81145789bf6cf
Icaro de Barros <icarodebarros@gmail.com>
2022-02-25 14:36:25 -0300

    Using the Transition component

--- src/App.js
+++ src/App.js
@@ -50,9 +50,15 @@ class App extends Component {
             />
           )}
         </Transition>
-        {this.state.modalIsOpen ? (
-          <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-        ) : null}
+        <Transition 
+          mountOnEnter
+          unmountOnExit  
+          in={this.state.modalIsOpen}
+          timeout={300}>
+          {state => (
+            <Modal show={state} closed={this.closeModal} />
+          )}
+        </Transition>
         {this.state.modalIsOpen ? (
           <Backdrop show={this.state.modalIsOpen} />
         ) : null}
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,7 +5,8 @@ import "./Modal.css";
 const modal = props => {
   const cssClasses = [
     "Modal",
-    props.show ? "ModalOpen" : "ModalClosed"
+    props.show === "entering" ? "ModalOpen" : 
+    props.show === "exiting" ? "ModalClosed" : null
   ];
 
   return (
